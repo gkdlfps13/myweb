2eca52a2cc3bdec2d964f5cedbd9b97d59a957d2
gkdlfps13 <69507662+gkdlfps13@users.noreply.github.com>
2020-08-11 21:02:19 +0900

    day1sumin2

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React from 'react';
+impimport React from 'react';
 import logo from './logo.svg';
 import './App.css';
-{Button, Progress} from 'semantic-ui-react'
+import {Button, Progress} from 'semantic-ui-react'
 
 function App() {
   return (
     <div>
     <p> 안녕하 세요 ! </p>
     <Button Primary> hi </Button>
-    <Progress percent = {33}/>
+    <Progress percent = {33} indicating/>
     </div>
   );
 }
